3c848ae2178b3e834c3f61e1742f4c421c9bef97
ProtaSymat <giraultmathys@gmail.com>
2024-04-12 14:26:58 +0200

    création Taskfixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,7 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Step;
 use App\Entity\Board;
 use App\Entity\Task;
-use App\Entity\TaskHistory;
+use App\Entity\History;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -14,42 +14,6 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $faker = Factory::create();
-
-        for ($i = 0; $i < 5; $i++) {
-            $step = new Step();
-            $step->setTitle($faker->sentence())
-                ->setPosition($i)
-                ->setCreatedAt($faker->dateTimeThisMonth())
-                ->setUpdatedAt($faker->dateTimeThisMonth());
-            $manager->persist($step);
-
-            $board = new Board();
-            $board->setTitle($faker->sentence(3))
-                ->setCreatedAt($faker->dateTimeThisMonth())
-                ->setUpdatedAt($faker->dateTimeThisMonth())
-                ->addStep($step);
-            $manager->persist($board);
-
-            for ($j = 0; $j < 5; $j++) {
-                $task = new Task();
-                $task->setTitle($faker->sentence())
-                    ->setStatus($faker->randomElement(['1', '2', '3']))
-                    ->setCreatedAt($faker->dateTimeThisMonth())
-                    ->setUpdatedAt($faker->dateTimeThisMonth())
-                    ->setStep($step);
-                $manager->persist($task);
-
-                $history = new TaskHistory();
-                $history->setEvent($faker->sentence())
-                    ->setEventDate($faker->dateTimeThisMonth())
-                    ->setCreatedAt($faker->dateTimeThisMonth())
-                    ->setDetails($faker->sentence())
-                    ->setTask($task);
-                $manager->persist($history);
-            }
-        }
-
-        $manager->flush();
+        
     }
 }
---
+++ src/DataFixtures/TaskFixtures.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\DataFixtures;
+
+use App\Entity\Step;
+use App\Entity\Board;
+use App\Entity\Task;
+use App\Entity\TaskHistory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class TaskFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 5; $i++) {
+            $board = new Board();
+            $board->setTitle($faker->sentence(3))
+                  ->setCreatedAt($faker->dateTimeThisMonth())
+                  ->setUpdatedAt($faker->dateTimeThisMonth());
+
+            $manager->persist($board);
+
+            $step = new Step();
+            $step->setTitle($faker->sentence())
+                 ->setPosition($i)
+                 ->setCreatedAt($faker->dateTimeThisMonth())
+                 ->setUpdatedAt($faker->dateTimeThisMonth());
+
+            $manager->persist($step);
+
+            
+
+            for ($j = 0; $j < 5; $j++) {
+                $task = new Task();
+                $task->setTitle($faker->sentence())
+                     ->setStatus($faker->randomElement(['todo', 'in_progress', 'done']))
+                     ->setCreatedAt($faker->dateTimeThisMonth())
+                     ->setUpdatedAt($faker->dateTimeThisMonth())
+                     ->setStep($step);
+
+
+                $manager->persist($task);
+
+                $history = new TaskHistory();
+                $history->setEvent($faker->sentence())
+                        ->setEventDate($faker->dateTimeThisMonth())
+                        ->setCreatedAt($faker->dateTimeThisMonth())
+                        ->setDetails($faker->sentence())
+                        ->setTask($task);
+
+                $manager->persist($history);
+            }
+        }
+
+        $manager->flush();
+    }
+}
