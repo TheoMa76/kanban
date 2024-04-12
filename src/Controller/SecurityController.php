a1de26b7a455e1d0fc4b65cee936df9df000cfc8
Adambizien/blog-symfony <bizienadam@outlook.com>
2024-04-12 11:39:42 +0200

    :adhesive_bandage: fix redirection after login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,10 +18,15 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ]);
+        if($error){
+            return $this->render('security/login.html.twig', [
+                'last_username' => $lastUsername,
+                'error' => $error,
+            ]);
+        }else{
+            return $this->redirectToRoute('app_home');
+        }
+        
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
