76cd6d791e226770b43f383642409069574a5f97
Dezyop2 <wlebouvier@normandiewebschool.fr>
2024-04-12 11:46:41 +0200

    fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,54 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Step;
+use App\Entity\Board;
+use App\Entity\Task;
+use App\Entity\History;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 5; $i++) {
+            $step = new Step();
+            $step->setTitle($faker->sentence())
+                ->setPosition($i)
+                ->setCreatedAt($faker->dateTimeThisMonth())
+                ->setModifiedAt($faker->dateTimeThisMonth());
+            $manager->persist($step);
+
+            $board = new Board();
+            $board->setTitle($faker->sentence(3))
+                ->setCreatedAt($faker->dateTimeThisMonth())
+                ->setModifiedAt($faker->dateTimeThisMonth())
+                ->addStep($step); 
+            $manager->persist($board);
+
+            for ($j = 0; $j < 5; $j++) {
+                $task = new Task();
+                $task->setTitle($faker->sentence())
+                    ->setDescription($faker->paragraph())
+                    ->setPriority($faker->randomElement(['low', 'medium', 'high']))
+                    ->setStatus($faker->randomElement(['1', '2', '3']))
+                    ->setCreatedAt($faker->dateTimeThisMonth())
+                    ->setModifiedAt($faker->dateTimeThisMonth())
+                    ->setStep($step);
+                $manager->persist($task);
+
+                $history = new History();
+                $history->setEvent($faker->sentence())
+                    ->setEventDate($faker->dateTimeThisMonth())
+                    ->setDetails($faker->sentence())
+                    ->setTask($task);
+                $manager->persist($history);
+            }
+        }
 
         $manager->flush();
     }
